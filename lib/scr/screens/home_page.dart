e541f22345ff0820e7389ae55dd1669483afba5d
NorqobilovZafarbek <zafarbeknorqobilov360@gmail.com>
2023-08-11 12:35:02 +0500

    OverflowBox

--- lib/scr/screens/home_page.dart
+++ lib/scr/screens/home_page.dart
@@ -11,14 +11,27 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: UnconstrainedBox(
-        child: LimitedBox(
-          maxHeight: 150,
-          maxWidth: 150,
-          child: Container(
-            width: 5000,
-            height: 5000,
-            color: Colors.red,
+      body: Center(
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: DecoratedBox(
+            decoration: const BoxDecoration(color: Colors.red),
+            child: OverflowBox(
+              maxWidth: 200,
+              maxHeight: 200,
+              minWidth: 200,
+              minHeight: 200,
+              child: SizedBox(
+                height: 400,
+                width: 400,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.red.withOpacity(.5),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
