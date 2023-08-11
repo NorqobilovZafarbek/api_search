2ac1bd45e442ac6ee6763444c2824f59feb32afd
NorqobilovZafarbek <zafarbeknorqobilov360@gmail.com>
2023-08-11 12:41:12 +0500

    SizedOverflowBox

--- lib/scr/screens/home_page.dart
+++ lib/scr/screens/home_page.dart
@@ -13,23 +13,15 @@ class _HomePage extends State<HomePage> {
     return Scaffold(
       body: Center(
         child: SizedBox(
-          height: 100,
-          width: 100,
           child: DecoratedBox(
-            decoration: const BoxDecoration(color: Colors.red),
-            child: OverflowBox(
-              maxWidth: 200,
-              maxHeight: 200,
-              minWidth: 200,
-              minHeight: 200,
-              child: SizedBox(
-                height: 400,
-                width: 400,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Colors.red.withOpacity(.5),
-                  ),
-                ),
+            decoration: const BoxDecoration(
+              color: Colors.red,
+            ),
+            child: SizedOverflowBox(
+              size: const Size(50, 50),
+              child: ElevatedButton(
+                child: const Text("ElevatedButton"),
+                onPressed: () {},
               ),
             ),
           ),
