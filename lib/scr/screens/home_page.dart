8dd15da1e4c3c6b5b05ab5b16646c498528f95e9
NorqobilovZafarbek <zafarbeknorqobilov360@gmail.com>
2023-08-14 17:07:54 +0500

    TabBarView

--- lib/scr/screens/home_page.dart
+++ lib/scr/screens/home_page.dart
@@ -9,32 +9,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePage extends State<HomePage> {
   int index = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox.expand(
-          child: DraggableScrollableSheet(
-            initialChildSize: .3,
-            minChildSize: .13,
-            maxChildSize: .8,
-            builder: (BuildContext context, ScrollController scrollController) {
-              return Container(
-                color: Colors.blue[100],
-                child: ListView.builder(
-                  controller: scrollController,
-                  itemCount: 10000,
-                  itemBuilder: (BuildContext context, int index) {
-                    return DecoratedBox(decoration: BoxDecoration(color: Colors.primaries[index % 15]),
-                      child: Text("4ifikfm4rf"),
-                    );
-                  },
-                ),
-              );
-            },
-          ),
+    return const DefaultTabController(
+      length: 3,
+      animationDuration: Duration(milliseconds: 200),
+      child: Scaffold(
+        appBar: TabBar(
+          tabs: [
+            Text("1"),
+            Text("2"),
+            Text("3"),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Scaffold(backgroundColor: Colors.black),
+            Scaffold(backgroundColor: Colors.purple),
+            Scaffold(backgroundColor: Colors.deepPurpleAccent),
+          ],
         ),
       ),
     );
