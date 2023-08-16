fddd57e65d0816bd4d67937b6a78aa102f2bb332
NorqobilovZafarbek <zafarbeknorqobilov360@gmail.com>
2023-08-16 10:37:43 +0500

    Slidable : ListTileni surganda O'chishi yoki ..

--- lib/scr/screens/home_page.dart
+++ lib/scr/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,25 +9,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  final List<int> items = List<int>.generate(30, (index) => index);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverFixedExtentList(
-            itemExtent: 100.0,
-            delegate: SliverChildBuilderDelegate(
-              childCount: 100,
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: Text('list item $index'),
-                );
-              },
+      body: ListView.separated(
+        itemCount: 20,
+        separatorBuilder: (BuildContext context, int index) {
+          return Slidable(
+            key: UniqueKey(),
+            startActionPane: ActionPane(
+              dismissible: DismissiblePane(
+                onDismissed: () {},
+              ),
+              motion: const BehindMotion(),
+              children: [
+                SlidableAction(
+                  label: "DELETE",
+                  backgroundColor: Colors.red,
+                  onPressed: (context) => {},
+                  icon: Icons.delete,
+                ),
+                SlidableAction(
+                  backgroundColor: Colors.blue,
+                  onPressed: (context) => {},
+                  icon: Icons.share,
+                ),
+              ],
             ),
-          )
-        ],
+            endActionPane: ActionPane(
+              motion: const DrawerMotion(),
+              children: [
+                SlidableAction(
+                  backgroundColor: Colors.green,
+                  onPressed: (context) => {},
+                  icon: Icons.call,
+                ),
+                SlidableAction(
+                  backgroundColor: Colors.blue,
+                  onPressed: (context) => {},
+                  icon: Icons.message,
+                ),
+              ],
+            ),
+            child: ListTile(
+              title: Text("Zafarbek"),
+              subtitle: Text("Norqobilov"),
+            ),
+          );
+        },
+        itemBuilder: (context, index) {
+          return SizedBox();
+        },
       ),
     );
   }
